6d34c51c1537375771a238dfdf35cde19f38848d
Laptop 122 <Laptop 122@LAPTOP122>
2025-07-16 18:29:49 -0500

    � prepare for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // 👉 this must match your repo name
+  base: "/test_repo/",
+  build: {
+    // output into docs/ for GitHub Pages
+    outDir: "docs",
+  },
+});
