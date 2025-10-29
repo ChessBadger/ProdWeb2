9cb5e81d988c61e6c122951099b18a7c4710b8b2
Laptop 122 <Laptop 122@LAPTOP122>
2025-10-29 12:05:48 -0500

    Auto-update: Excel→JSON, copied, built & pushed

--- src/App.tsx
+++ src/App.tsx
@@ -40,8 +40,6 @@ const allowedUsers = [
   "kgrohall@badgerinventory.com",
   "files@badgerinventory.com",
   "qianabatton@gmail.com",
-  "QIANABATTON@GMAIL.COM",
-  "CLARKLAS831@GMAIL.COM",
   "clarklas831@gmail.com",
 ];
 
