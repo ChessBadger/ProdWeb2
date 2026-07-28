5de78396acd4c2d559f8518cc027f5c8918e405c
Laptop 122 <Laptop 122@LAPTOP122>
2026-07-28 10:54:28 -0500

    Auto-update: Excel to JSON, copied, built & pushed

--- public/scheduleplanningapp.js
+++ public/scheduleplanningapp.js
@@ -268,7 +268,7 @@ const ANALYTICS_DB_NAME = "crew_predictor_analytics";
 const ANALYTICS_DB_VERSION = 1;
 const ANALYTICS_DB_STORE = "snapshots";
 const ANALYTICS_DB_SNAPSHOT_ID = "latest";
-const DATA_ASSET_VERSION = "20260719-142716";
+const DATA_ASSET_VERSION = "20260728-105408";
 const withDataAssetVersion = (path) => `${path}?v=${DATA_ASSET_VERSION}`;
 const HISTORY_JSON_PATH = withDataAssetVersion("data/EmployeeProductionExport.json");
 const ACTIVE_EMPLOYEE_JSON_PATH = withDataAssetVersion("data/EmployeeProductionExport.json");
